40f439c36d86778988449c8ad97c75e1cf56c527
bridge <liangnanqiao@qq.com>
2023-05-23 20:12:49 +0800

    todo4

--- src/main/java/io/pivotal/literx/Part01Flux.java
+++ src/main/java/io/pivotal/literx/Part01Flux.java
@@ -35,7 +35,7 @@ public class Part01Flux {
 
 	// TODO Create a Flux that emits an IllegalStateException
 	Flux<String> errorFlux() {
-		return null;
+		return Flux.error(new Throwable(new IllegalStateException()));
 	}
 
 //========================================================================================
